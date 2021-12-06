a3c8f65e8f3544d57b27e69d9ffc04c26556b88d
Rana Jurjus <rjurjus11@mail.bw.edu>
2021-12-06 17:33:52 -0500

    Continue TDD between recipe class and recipe_test

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -1,16 +1,23 @@
+const Ingredient = require("../src/data/ingredients.js");
+
 class Recipe {
   constructor(id, image, ingredients, instructions, name, tags) {
     this.id = id;
-    this.image = image || "";
-    this.ingredients = ingredients || [];
-    this.instructions = instructions || [];
-    this.name = name || "";
-    this.tags = tags || [];
+    this.image = image;
+    this.ingredients = ingredients;
+    this.instructions = instructions;
+    this.name = name;
+    this.tags = tags;
   }
   determineIngredients() {
     return this.ingredients;
   }
-  getCostOfIngredients() {}
+  getCostOfIngredients() {
+    this.ingredients.estimatedCostInCents * this.ingredients.quantity["amount"];
+    // get cost of each ingredient (estimatedCostInCents * amount)
+    // add all ingredients final costs together
+    // return total cost for recipe
+  }
   getInstructions() {}
 }
 
