e5746622816b3a873fa0c7864df50ff233f249a7
Rana Jurjus <rjurjus11@mail.bw.edu>
2021-12-08 23:44:30 -0500

    Pull in recipe data and update innerHTML function

--- src/scripts.js
+++ src/scripts.js
@@ -1,50 +1,72 @@
-import './styles.css';
-import apiCalls from './apiCalls';
-import Cookbook from './classes/Cookbook.js';
+import "./styles.css";
+import { recipeData } from "./data/recipes.js";
+import { ingredientsData } from "./data/ingredients.js";
+import Ingredient from "./classes/Ingredient.js";
+import apiCalls from "./apiCalls";
+import Cookbook from "./classes/Cookbook.js";
+import Recipe from "./classes/Recipe.js";
 
+const recipeTitle = document.querySelector("#openRecipe");
+const homePage = document.querySelector(".home-page");
+const recipeView = document.querySelector(".recipe-view");
+const homeButton = document.querySelector(".home-button");
+const recipeCard = document.querySelector(".recipe-card");
 
-const recipeTitle = document.querySelector('#openRecipe');
-const homePage = document.querySelector('.home-page');
-const recipeView = document.querySelector('.recipe-view');
-const homeButton = document.querySelector('.home-button');
-const recipeCard = document.querySelector('.recipe-card');
-let cookbook = new CookBook();
+const recipes = recipeData.map(
+  ({ id, image, ingredients, instructions, name, tags }) => {
+    const ingredientObjects = ingredients.map(({ id, quantity }) => {
+      const { name, estimatedCostInCents } = ingredientsData.find(
+        (ingredientData) => ingredientData.id === id
+      );
+      return new Ingredient({ id, name, estimatedCostInCents, quantity });
+    });
+    return new Recipe(id, image, ingredientObjects, instructions, name, tags);
+  }
+);
 
+let cookbook = new Cookbook(recipes);
+// console.log(cookbook.filterTags(["snack"]));
 
 function addHidden(element) {
-  element.classList.add('hidden');
-};
-
+  element.classList.add("hidden");
+}
 
 function removeHidden(element) {
-  element.classList.remove('hidden');
-};
+  element.classList.remove("hidden");
+}
 
 function displayRecipeView() {
   addHidden(homePage);
   removeHidden(recipeView);
   removeHidden(homeButton);
-};
+}
 
 function populateRecipes() {
-  let recipeCardsHTML = '';
-  cookbook.recipes.forEach(recipe => {
-    recipeCardsHTML += `
-    <img class="" id="" src="${recipe.image}" alt="recipe image">
-    <h1 class="recipe-title" id="openRecipe">${recipe.name}</h1>`
+  homePage.innerHTML = cookbook.recipes
+    .map((recipe) => {
+      return `
+    <article class="recipe-card">
+        <img class="" src="${recipe.image}" alt="Image of ${
+        recipe.name
+      }" width=400>
+        <h1 class="recipe-title" data-recipe-id="${recipe.id}">${
+        recipe.name
+      }</h1>
+        <div>
+          <button class="">Favorite Button</button>
+          <p>Tags: ${recipe.tags.join(", ")}</p>
+        </div>
+      </article>`;
+    })
+    .join("");
+  document.querySelectorAll(".recipe-title").forEach((recipeTitle) => {
+    recipeTitle.addEventListener("click", (event) => {
+      const recipeId = parseInt(event.target.dataset.recipeId);
+      console.log(recipes.find(({ id }) => id === recipeId));
+      displayRecipeView();
+    });
   });
-  recipeCard.innerHTML = recipeCardsHTML;
-};
+}
 populateRecipes();
 
-
-recipeTitle.addEventListener('click', displayRecipeView);
-
-
-
-
-
-
-
-
-console.log('Hello world');
+console.log("Hello world");
