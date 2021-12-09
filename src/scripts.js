e54007319e47163ded19d1d2153608a8b43787b6
Emily Corcoran <cathleencorcoran@gmail.com>
2021-12-08 19:59:08 -0500

    Build CSS for selecting recipe title on homepage, and hiding homepage view and displaying recipe view

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,31 @@
 import './styles.css';
 import apiCalls from './apiCalls';
 
+const recipeTitle = document.querySelectorAll('#openRecipe');
+const homePage = document.querySelector('.home-page');
+const recipeView = document.querySelector('recipe-view');
+
+
+function addHidden(element) {
+  element.classList.add('hidden');
+};
+
+
+function removeHidden(element) {
+  element.classList.remove('hidden');
+};
+
+
+function displayRecipeView() {
+  addHidden(homePage);
+  removeHidden(recipeView);
+};
+
+
+
+recipeTitle.addEventListener('click', displayRecipeView);
+
+
 
 
 
