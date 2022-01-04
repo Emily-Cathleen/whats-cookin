735b5fa5c8a4c3e2fa61cbbe3a34fe3372953146
Rana Jurjus <rjurjus11@mail.bw.edu>
2022-01-04 13:50:47 -0500

    Begin to build out pantry test

---
+++ src/classes/Pantry.js
@@ -0,0 +1,18 @@
+class Pantry {
+  constructor(pantryList, ingredientList) {
+    this.ingredientList = ingredientList;
+    // this.data = pantryList.map(...);
+  }
+  // getMissingIngredients(){}
+  // cookAMeal(){}
+  // getAllIngredients()
+  // addIngredients()
+}
+
+module.exports = Pantry;
+
+// pantryList : [{ingredient: number, amount: number}]
+// ingredientList: [{id: number, name: string, estimatedCostInCents: number}]
+// what we want to store: [{id: number, name: string, amount: number}]
+
+// WHAT DOES IT MEAN TO HAVE 5 FLOUR? 😩
