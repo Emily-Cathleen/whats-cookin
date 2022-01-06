e243ddecf5b55b6d1b39a9433905556d19cfe098
Susanna Carey <susannaopal@gmail.com>
2022-01-06 13:40:03 -0500

    Update API links and remove second .then in the fAPI file and run it through Promise function instead

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,22 +1,19 @@
 export const fetchUsers = () => {
-  return fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users")
+  return fetch("http://localhost:3001/api/v1/users")
     .then((response) => response.json())
-    .then((data) => data.usersData)
     .catch((error) => console.log(error));
 };
 
 export const fetchIngredients = () => {
   return fetch(
-    "https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients"
+    "http://localhost:3001/api/v1/ingredients	"
   )
     .then((response) => response.json())
-    .then((data) => data.ingredientsData)
     .catch((error) => console.log(error));
 };
 
 export const fetchRecipes = () => {
-  return fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes")
+  return fetch("http://localhost:3001/api/v1/recipes")
     .then((response) => response.json())
-    .then((data) => data.recipeData)
     .catch((error) => console.log(error));
 };
