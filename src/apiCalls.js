c6e91ea262249d5a0df4f7a5ce79dc98f2c0dfe1
Susanna Carey <susannaopal@gmail.com>
2022-01-05 20:54:34 -0500

    Update API calls to work with Promise.all

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,26 +1,22 @@
-// Your fetch requests will live here!
-
- const fetchUsers = () => {
+export const fetchUsers = () => {
   return fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users")
     .then((response) => response.json())
-    // .then((data) => data.usersData)
-    // .catch((error) => console.log(error));
+    .then((data) => data.usersData)
+    .catch((error) => console.log(error));
 };
 
- const fetchIngredients = () => {
+export const fetchIngredients = () => {
   return fetch(
     "https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients"
   )
     .then((response) => response.json())
-    // .then((data) => data.ingredientsData)
-    // .catch((error) => console.log(error));
+    .then((data) => data.ingredientsData)
+    .catch((error) => console.log(error));
 };
 
- const fetchRecipes = () => {
+export const fetchRecipes = () => {
   return fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes")
     .then((response) => response.json())
-    // .then((data) => data.recipeData)
-    // .catch((error) => console.log(error));
+    .then((data) => data.recipeData)
+    .catch((error) => console.log(error));
 };
-
-export { fetchUsers, fetchIngredients, fetchRecipes }
